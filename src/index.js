e954ea13efead386b2c2210ef081be5a37e6af12
wxnacy <371032668@qq.com>
2018-02-03 15:42:47 +0800

    index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import moment from 'moment';
+import { DatePicker, version } from 'antd';
+import 'antd/dist/antd.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <div style={{ margin: 24 }}>
+    <p style={{ marginBottom: 24 }}>
+      Current antd version: {version} <br />
+      You can change antd version on the left panel (Dependencies section).
+    </p>
+    <div></div>
+    
+    <DatePicker defaultValue={moment()} />
+  </div>,
+  document.getElementById('root')
+);
