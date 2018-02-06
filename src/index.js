5888dcea1ac9d2200d4ef70fbe873fed4e0a1f2e
wenxiaoning <wenxiaoning@gochinatv.com>
2018-02-06 22:26:07 +0800

    add

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import registerServiceWorker from './registerServiceWorker';
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import asyncComponent from './AsyncComponent';
@@ -16,21 +15,3 @@ ReactDOM.render(
     </Switch>
   </BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
-=======
-import moment from 'moment';
-import { DatePicker, version } from 'antd';
-import 'antd/dist/antd.css';
-
-ReactDOM.render(
-  <div style={{ margin: 24 }}>
-    <p style={{ marginBottom: 24 }}>
-      Current antd version: {version} <br />
-      You can change antd version on the left panel (Dependencies section).
-    </p>
-    <div></div>
-    
-    <DatePicker defaultValue={moment()} />
-  </div>,
-  document.getElementById('root')
-);
->>>>>>> e954ea13efead386b2c2210ef081be5a37e6af12
